1ba1e2fca92cf6943b13dd75d29ad2303007fab0
rizzla82 <rizzla82@hotmail.com>
2021-09-10 21:52:33 +0100

    Master Upload

---
+++ Event-Find.ps1
@@ -0,0 +1,7 @@
+﻿cd C:\data\PowerShell\reports
+
+$StartTime=Get-Date -Year 2019 -Month 3 -Day 25 -Hour 00 -Minute 00
+
+$EndTime=Get-Date -Year 2019 -Month 3 -Day 25 -Hour 23 -Minute 59
+
+Get-WinEvent -FilterHashtable @{LogName='System';;ID='18';StartTime=$StartTime;EndTime=$EndTime} | Export-Csv -Path .\25-03-2019.Csv -NoTypeInformation
---
+++ Remove-7zip.ps1
@@ -0,0 +1,4 @@
+﻿$b = Get-WmiObject -Class win32_product -ComputerName localhost, zfs10.gsldev.local -Filter "Name like '%7-Zip%'"
+$b.Uninstall()
+
+
